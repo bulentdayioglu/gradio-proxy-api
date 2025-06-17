905b0b2b5c33e075236d409fc5cc01f42800377b
bulentdayioglu <161126100+bulentdayioglu@users.noreply.github.com>
2025-06-17 17:31:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from gradio_client import Client, handle_file
 
 app = Flask(__name__)
+
+client = Client("AAAAA12344321/GardenGuard")  # Gradio Space ID doğru yaz!
 
 @app.route('/')
 def index():
@@ -11,9 +14,13 @@ def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'}), 400
     
-    # Dosya bilgilerini geri döndür (sadece test)
     file = request.files['image']
-    return jsonify({'filename': file.filename, 'content_type': file.content_type})
+    result = client.predict(
+        image=handle_file(file),
+        api_name="/predict"
+    )
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
+
